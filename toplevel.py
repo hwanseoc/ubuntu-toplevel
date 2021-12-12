14ad7cbc5b67c6879df438346b58a3fe7d0ef2ee
Henry Choi <4735067+fluxt@users.noreply.github.com>
2021-12-12 17:51:29 -0600

    updated

--- toplevel.py
+++ toplevel.py
@@ -8,40 +8,36 @@ def replace_arch(name):
     name = re.sub('(:[a-zA-Z0-9_]*)', '', name)
     return name
 
-response = requests.get('https://releases.ubuntu.com/21.04/ubuntu-21.04-desktop-amd64.manifest')
+response = requests.get('https://releases.ubuntu.com/20.04/ubuntu-20.04.3-desktop-amd64.manifest')
 text = response.text
 manifest = [
     replace_arch(line.split('\t')[0]) for line in text.split('\n')
 ]
 
-installed = [
-    replace_arch(pkg.name) for pkg in cache.Cache() if pkg.is_installed
-]
+# installed = [
+#     replace_arch(pkg.name) for pkg in cache.Cache() if pkg.is_installed
+# ]
 
 manual = [
     replace_arch(pkg.name) for pkg in cache.Cache() if pkg.is_installed and not pkg.is_auto_installed
 ]
 
-depends = [
-    replace_arch(dep_pkg.name)
-    for pkg in cache.Cache() if pkg.is_installed
-    for dep in pkg.installed.get_dependencies('PreDepends', 'Depends', 'Recommends')
-    for dep_pkg in dep
-]
+# depends = [
+#     replace_arch(dep_pkg.name)
+#     for pkg in cache.Cache() if pkg.is_installed
+#     for dep in pkg.installed.get_dependencies('PreDepends', 'Depends', 'Recommends')
+#     for dep_pkg in dep
+# ]
 
-depends_edge = [
-    [replace_arch(pkg.name), replace_arch(dep_pkg.name)]
-    for pkg in cache.Cache() if pkg.is_installed
-    for dep in pkg.installed.get_dependencies('PreDepends', 'Depends', 'Recommends')
-    for dep_pkg in dep
-]
+# depends_edge = [
+#     [replace_arch(pkg.name), replace_arch(dep_pkg.name)]
+#     for pkg in cache.Cache() if pkg.is_installed
+#     for dep in pkg.installed.get_dependencies('PreDepends', 'Depends', 'Recommends')
+#     for dep_pkg in dep
+# ]
 
 # %%
-manual_not_manifest = [name for name in manual if name not in manifest]
-manual_not_manifest = sorted(manual_not_manifest)
+manual_not_manifest = sorted([name for name in manual if name not in manifest])
 
 for name in manual_not_manifest:
-    # if name in depends:
-    #     print(name)
-    if '-ko' not in name and '-cjk-' not in name:
-            print(name)
+    print(name)
