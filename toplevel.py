84c0b26dea07a0e888e1b970c3ae6141a7ec338f
Hwanseo Choi <electricvoxel@gmail.com>
2025-02-28 01:31:09 -0800

    .

--- toplevel.py
+++ toplevel.py
@@ -1,7 +1,8 @@
 from apt import cache
 import requests
 
-response = requests.get('https://releases.ubuntu.com/24.04.1/ubuntu-24.04.1-desktop-amd64.manifest')
+response = requests.get('https://old-releases.ubuntu.com/releases/noble/ubuntu-24.04.1-desktop-amd64.manifest')
+
 assert response.ok
 manifest = set(line.split('\t')[0] for line in response.text.split('\n'))
 manual = set(pkg.name for pkg in cache.Cache() if pkg.is_installed and not pkg.is_auto_installed)
