dcfcc5ae135a5f8e64d8876b1651c14af6a8cce5
Hwanseo Choi <electricvoxel@gmail.com>
2025-08-09 05:40:16 +0900

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+
+```
+python3 -m venv venv --system-site-packages
+
+source venv/bin/activate
+
+pip install networkx
+
+python3 toplevel.py
+```
--- toplevel.py
+++ toplevel.py
@@ -1,9 +1,9 @@
 from apt import cache
 import requests
 
-# https://old-releases.ubuntu.com/releases/noble/ubuntu-24.04.1-desktop-amd64.manifest
-# https://releases.ubuntu.com/releases/24.04.2/ubuntu-24.04.2-desktop-amd64.manifest
-response = requests.get('https://releases.ubuntu.com/releases/24.04.2/ubuntu-24.04.2-desktop-amd64.manifest')
+# https://old-releases.ubuntu.com/releases/noble/ubuntu-24.04.2-desktop-amd64.manifest
+# https://releases.ubuntu.com/releases/24.04.3/ubuntu-24.04.3-desktop-amd64.manifest
+response = requests.get('https://releases.ubuntu.com/releases/24.04.3/ubuntu-24.04.3-desktop-amd64.manifest')
 
 assert response.ok
 manifest = set(line.split('\t')[0] for line in response.text.split('\n'))
